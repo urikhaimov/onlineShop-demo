9d3e56f79fb213b90c96568a5007e657c532cee6
Uri Khaimov <uri.khaimov@gmail.com>
2025-07-31 16:09:38 +0300

    feat: update TestPage for responsive layout with media query support

--- apps/client/src/pages/TestPage.tsx
+++ apps/client/src/pages/TestPage.tsx
@@ -1,11 +1,13 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { drawerWidth } from '../constants/globalConstants';
 
 const TestPage = () => {
+  const muiTheme = useTheme();
+  const isMobile = useMediaQuery(muiTheme.breakpoints.down('sm'));
   return (
     <Box
       sx={{
-        width: `calc(100vw - ${drawerWidth}px)`,
+        width: isMobile ? '100vw' : `calc(100vw - ${drawerWidth}px)`,
         height: '2000px', // simulate long scroll
         backgroundColor: '#f0f0f0',
         p: 3,
