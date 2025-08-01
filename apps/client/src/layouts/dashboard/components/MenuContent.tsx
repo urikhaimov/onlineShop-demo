b3dcaf6511516abb3bf1f24201c5fafbde95816e
Uri Khaimov <uri.khaimov@gmail.com>
2025-08-01 11:22:10 +0300

    feat: refactor MenuContent component for improved imports and structure

--- apps/client/src/layouts/dashboard/components/MenuContent.tsx
+++ apps/client/src/layouts/dashboard/components/MenuContent.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Stack from '@mui/material/Stack';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+} from '@mui/material';
 import {
   AccountCircle as AccountCircleIcon,
   AdminPanelSettings as AdminPanelSettingsIcon,
@@ -15,21 +17,21 @@ import {
   People as PeopleIcon,
   Receipt as ReceiptIcon,
   Security as SecurityIcon,
+  SettingsRounded as SettingsRoundedIcon,
+  InfoRounded as InfoRoundedIcon,
+  HelpRounded as HelpRoundedIcon,
 } from '@mui/icons-material';
-import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
-import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
-import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+
 import { useAuth } from '../../../hooks/useAuth';
 import { isAdmin } from '../../../context/AuthContext';
 
 export default function MenuContent() {
   const { role } = useAuth();
+
   const mainListItems = [
     { label: 'Home', icon: <HomeIcon />, path: '/' },
-
     { label: 'Products', icon: <InventoryIcon />, path: '/products' },
     { label: 'My Orders', icon: <ReceiptIcon />, path: '/my-orders' },
-    // { label: 'Profile', icon: <AccountCircleIcon />, path: '/profile' },
   ];
 
   const secondaryListItems = isAdmin(role)
@@ -44,10 +46,26 @@ export default function MenuContent() {
           icon: <CategoryIcon />,
           path: '/admin/categories',
         },
-        { label: 'Users', icon: <PeopleIcon />, path: '/admin/users' },
-        { label: 'Products', icon: <InventoryIcon />, path: '/admin/products' },
-        { label: 'Orders', icon: <ReceiptIcon />, path: '/admin/orders' },
-        { label: 'Theme', icon: <BrushIcon />, path: '/admin/theme' },
+        {
+          label: 'Users',
+          icon: <PeopleIcon />,
+          path: '/admin/users',
+        },
+        {
+          label: 'Products',
+          icon: <InventoryIcon />,
+          path: '/admin/products',
+        },
+        {
+          label: 'Orders',
+          icon: <ReceiptIcon />,
+          path: '/admin/orders',
+        },
+        {
+          label: 'Theme',
+          icon: <BrushIcon />,
+          path: '/admin/theme',
+        },
         {
           label: 'Landing Page',
           icon: <HomeIcon />,
@@ -60,27 +78,40 @@ export default function MenuContent() {
         },
       ]
     : [
-        { text: 'Settings', icon: <SettingsRoundedIcon /> },
-        { text: 'About', icon: <InfoRoundedIcon /> },
-        { text: 'Feedback', icon: <HelpRoundedIcon /> },
+        {
+          label: 'Settings',
+          icon: <SettingsRoundedIcon />,
+          path: '/settings',
+        },
+        {
+          label: 'About',
+          icon: <InfoRoundedIcon />,
+          path: '/about',
+        },
+        {
+          label: 'Feedback',
+          icon: <HelpRoundedIcon />,
+          path: '/feedback',
+        },
       ];
 
   return (
     <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
       <List dense>
         {mainListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
-            <ListItemButton selected={index === 0}>
+          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton selected={index === 0} href={item.path}>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.label} />
             </ListItemButton>
           </ListItem>
         ))}
       </List>
+
       <List dense>
-        {secondaryListItems.map((item, index) => (
-          <ListItem key={index} disablePadding sx={{ display: 'block' }}>
-            <ListItemButton>
+        {secondaryListItems.map((item) => (
+          <ListItem key={item.path} disablePadding sx={{ display: 'block' }}>
+            <ListItemButton href={item.path}>
               <ListItemIcon>{item.icon}</ListItemIcon>
               <ListItemText primary={item.label} />
             </ListItemButton>
