2f8f55420f8e8a542f939f1f66ee8594cfbbb880
Volodymyr Tkach <teamco@gmail.com>
2025-08-25 13:42:34 +0300

    feat: refactor Firebase admin initialization to use environment variables

--- libs/firebase/src/lib/firebase-admin.ts
+++ libs/firebase/src/lib/firebase-admin.ts
@@ -1,12 +1,25 @@
 import * as admin from 'firebase-admin';
-import serviceAccount from './service-account.json';
+import { cert } from 'firebase-admin/app';
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: 'onlinestoretemplate-59d3e.firebasestorage.app', // ✅ IMPORTANT!
+  credential: cert({
+    // @ts-expect-error - Provided by firebase.
+    type: process.env['FB_ADMIN_TYPE'],
+    project_id: process.env['FB_ADMIN_PROJECT_ID'],
+    private_key_id: process.env['FB_ADMIN_PRIVATE_KEY_ID'],
+    private_key: process.env['FB_ADMIN_PRIVATE_KEY']?.replace(/\\n/g, '\n'),
+    client_email: process.env['FB_ADMIN_CLIENT_EMAIL'],
+    client_id: process.env['FB_ADMIN_CLIENT_ID'],
+    auth_uri: process.env['FB_ADMIN_AUTH_URI'],
+    token_uri: process.env['FB_ADMIN_TOKEN_URI'],
+    auth_provider_x509_cert_url:
+      process.env['FB_ADMIN_AUTH_PROVIDER_X509_CERT_URL'],
+    client_x509_cert_url: process.env['FB_ADMIN_CLIENT_X509_CERT_URL'],
+    universe_domain: process.env['FB_ADMIN_UNIVERSE_DOMAIN'],
+  }),
 });
 
 const adminDb = admin.firestore();
 const adminAuth = admin.auth();
 
-export { adminDb, adminAuth, admin }; // ✅ export all necessary members
+export { adminDb, adminAuth, admin };
